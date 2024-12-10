4ba2db4fe94ebe3b05db9cd225e9d59ffd5dee8e
Valentin Köberl <valentin.koeberl@htl.rennweg.at>
2024-12-10 11:25:17 +0100

    first build

--- webtask/vite.config.js
+++ webtask/vite.config.js
@@ -1,3 +1,12 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/webtask/' : '/'
+});
+
+/**
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -16,3 +25,4 @@ export default defineConfig({
     },
   },
 })
+**/
